0ea09d7bd010d984ee1e80b0e33eba86f1948a9e
Ma6upa <yuvalerka@mail.ru>
2021-07-29 13:58:41 +0400

    Revert "Create redux support"
    This reverts commit 32bbda597245e7c6d7fa3448a1abda6a411bf6d4.

--- src/js/redux/actions.js
+++
@@ -1,9 +0,0 @@
-import { CHANGE_VALUE } from './actionTypes';
-
-export const changeValue = (content) => ({
-  type: CHANGE_VALUE,
-  payload: {
-    variable: content 
-  },
-})
-//+ '_newThingsAddedToTestVariableByRedux'
